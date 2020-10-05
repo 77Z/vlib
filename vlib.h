9ee4e199ecd8348e83375b3b14ab2b87ee6fd4a2
77Z <46038462+77Z@users.noreply.github.com>
2020-10-05 11:37:24 -0500

    Create vlib.h

---
+++ vlib.h
@@ -0,0 +1,13 @@
+//Copyright (c) 77Z 2020
+//Copyright (c) Vince Richter 2020
+
+#include <iostream>
+#include <string>
+
+#ifndef _VLIB_H
+#define _VLIB_H
+
+void print(std::string text);
+void println(std::string text);
+
+#endif
